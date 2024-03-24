273e76757408ac34da55b9cea34379530589a6ad
CHANGYU <window9u@konkuk.ac.kr>
2024-03-24 14:19:11 +0900

    refactor Client logic Main.java to Client.java

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,72 +1,17 @@
 package client;
-
-import client.component.Painting;
-import client.frame.MyFrame;
-import client.network.InputNetworkManager;
-import client.network.OutputNetworkManager;
-import type.response.*;
-
 import java.io.*;
 import java.net.Socket;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         int PORT = 9999;
-        ObjectInputStream in = null;
-        ObjectOutputStream out=null;
-        InputNetworkManager inputNetworkManager;
-        OutputNetworkManager outputNetworkManager;
-        try(Socket conn = new Socket("127.0.0.1",PORT)){
+        try (Socket conn = new Socket("127.0.0.1", PORT)) {
             System.out.println("Server started");
-            in=new ObjectInputStream(conn.getInputStream());
-            out=new ObjectOutputStream(conn.getOutputStream());
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-        PaintingManager pm = new PaintingManager();
-        new MyFrame(pm);
-        if(in==null || out==null){
-            return;
-        }
-        BlockingQueue<Painting> createResponse = new ArrayBlockingQueue<>(1);
-        BlockingQueue<Boolean> removeResponse = new ArrayBlockingQueue<>(1);
-        BlockingQueue<Boolean> selectResponse = new ArrayBlockingQueue<>(1);
-        BlockingQueue<Boolean> unselectResponse = new ArrayBlockingQueue<>(1);
-        inputNetworkManager= new InputNetworkManager(pm, createResponse, removeResponse, selectResponse, unselectResponse);
-        outputNetworkManager= new OutputNetworkManager(out, createResponse, removeResponse, selectResponse, unselectResponse);
-        pm.setOutputNetworkManager(outputNetworkManager);
-        //keep reading messages from the server
-        while(true){
-            try {
-                Response response = (Response) in.readObject();
-                if( response instanceof create){
-                    inputNetworkManager.create(((create) response));
-                } else if( response instanceof remove){
-                    inputNetworkManager.remove((remove) response);
-                } else if( response instanceof update){
-                    inputNetworkManager.update((update) response);
-                } else if (response instanceof select){
-                    inputNetworkManager.select((select) response);
-                } else if(response instanceof unselect){
-                    inputNetworkManager.unselect((unselect) response);
-                } else if(response instanceof  init){
-                    outputNetworkManager.init(((init) response).getCONNECTION_ID());
-                } else{
-                    System.out.println("Unknown message type");
-                    break;
-                }
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
-        try {
-            in.close();
-            out.close();
+            ObjectInputStream in = new ObjectInputStream(conn.getInputStream());
+            ObjectOutputStream out = new ObjectOutputStream(conn.getOutputStream());
+            new Client(in,out);
         } catch (IOException e) {
+            System.out.println("Server not started");
             e.printStackTrace();
         }
     }
