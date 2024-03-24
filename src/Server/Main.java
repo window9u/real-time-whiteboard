b4b6820184420b36e4e34ec4cb080a5cc62ea572
CHANGYU <window9u@konkuk.ac.kr>
2024-03-24 16:02:13 +0900

    refactor server main
    separate server listening part to Listener.java

--- src/Server/Main.java
+++ src/Server/Main.java
@@ -1,9 +1,7 @@
 package Server;
 
 import type.request.Request;
-import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -12,28 +10,18 @@ import java.util.concurrent.BlockingQueue;
 
 public class Main {
     public static void main(String[] args)  {
+        //config setting
         int PORT = 9999;
         int QUEUE_SIZE = 100;
         List<socketWriter> outList = Collections.synchronizedList(new ArrayList<>());
-         BlockingQueue<Request> requestQueue = new ArrayBlockingQueue<>(QUEUE_SIZE);
+        BlockingQueue<Request> requestQueue = new ArrayBlockingQueue<>(QUEUE_SIZE);
+
+        //start controller
         Controller controller = new Controller(outList, requestQueue);
         new Thread(controller).start();
-        try(ServerSocket serverSocket = new ServerSocket(PORT)){
-            System.out.println("Server started");
-            while(true){
-                try {
-                    Socket connection = serverSocket.accept();
-                    System.out.println("사용자접속: "+connection);
-                    ObjectOutputStream out = new ObjectOutputStream(connection.getOutputStream());
-                    controller.initConnection(out);
-                    new Thread(new Connection(connection, requestQueue)).start();
-                }catch (IOException e) {
-                    e.printStackTrace();
-                    break;
-                }
-            }
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+
+        //start server
+        Listener listener =new Listener(PORT, requestQueue);
+        listener.startListen(controller);
     }
 }
