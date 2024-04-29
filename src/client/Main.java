695edfa8d64d0b2e8ee6a25e44154bab06de4186
CHANGYU <window9u@konkuk.ac.kr>
2024-04-29 21:53:01 +0900

    feat: connect with server's IP

--- src/client/Main.java
+++ src/client/Main.java
@@ -10,8 +10,18 @@ public class Main {
             if(name==null || name.equals("exit") ){
                 break;
             }
-            getConnection(PORT, name);
+            getConnection(getHost(), PORT, name);
         }
+    }
+    private static String getHost(){
+        System.out.println("Enter the Server's ip (in same sub network): ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            return reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
     private static String getName() {
         System.out.println("Enter your Name: ");
@@ -24,9 +34,9 @@ public class Main {
         }
         return null;
     }
-    private static void getConnection(int PORT, String name){
+    private static void getConnection(String HOST,int PORT, String name){
         try {
-            Socket conn = new Socket("127.0.0.1", PORT);
+            Socket conn = new Socket(HOST, PORT);
             System.out.println("Server started");
             new Client(conn, name).run();
         } catch (IOException e) {
